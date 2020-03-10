86b8092f8a491bf752d2159ccbbdc5bcffb3c83e
Antonio Ruiz <antonioruiz@Antonios-MacBook-Air.local>
2020-03-10 23:11:38 +0100

    Add timer for the saving part.

--- joeynmt/training.py
+++ joeynmt/training.py
@@ -32,7 +32,7 @@ from joeynmt.data import load_data, make_data_iter
 from joeynmt.builders import build_optimizer, build_scheduler, \
     build_gradient_clipper
 from joeynmt.prediction import test
-
+from time import time
 
 # pylint: disable=too-many-instance-attributes
 class TrainManager:
@@ -300,7 +300,8 @@ class TrainManager:
             self.current_batch_multiplier = self.batch_multiplier
             count = self.current_batch_multiplier - 1
             epoch_loss = 0
-
+            
+            start_time = time()
             for i, batch in enumerate(iter(train_iter)):
                 # reactivate training
                 self.model.train()
@@ -390,6 +391,12 @@ class TrainManager:
                         ckpt_score = valid_score
 
                     new_best = False
+                    end_time = time()
+                    seconds_elapsed = end_time - start_time
+                    hours, rest = divmod(seconds_elapsed, 3600)
+                    if hours > 3:
+                    	self._save_checkpoint()
+                        start_time = time()
                     if self.is_best(ckpt_score):
                         self.best_ckpt_score = ckpt_score
                         self.best_ckpt_iteration = self.steps
